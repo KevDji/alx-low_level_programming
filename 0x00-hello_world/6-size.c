0c8e5c94b4b4d248b533d6b0c6feb841917f8341
KevDji <kevdjidadzie@gmail.com>
2022-06-30 18:56:50 -0700

    correction de tache quatrieme

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(k));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(v));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(n));
 	return (0);
 }
