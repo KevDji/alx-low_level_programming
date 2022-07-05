ca9a35642deba7aa35cc2361c994309b48d0ba33
KevDji <kevdjidadzie@gmail.com>
2022-07-05 16:48:06 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - Task 1
+ *
+ * Return: Always 0 (Success).
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar (alphabet);
+		alphabet++;
+
+	}
+	_putchar ('\n');
+
+}
