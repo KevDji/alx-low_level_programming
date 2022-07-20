ff7162d2bbc29e5b871d4a4956b312ec7d4ac728
KevDji <kevdjidadzie@gmail.com>
2022-07-20 10:17:41 -0700

    second  task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_sqrt(int n, int carre);
 int is_prime_number(int n);
+int is_prime(int n, int i);
 
 #endif /* MAIN_H */
