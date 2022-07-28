74266459d73b80e584b83309332e99399e4c41bd
KevDji <kevdjidadzie@gmail.com>
2022-07-28 13:38:10 -0700

    commit and correction advanced

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,8 @@ char *string_nconcat(char *s1, char s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void free(void *ptr);
+void exit(int status);
+void *malloc(size_t size);
+int *array_range(int min, int max);
 
 #endif /* MAIN_H */
