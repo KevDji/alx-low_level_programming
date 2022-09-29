be8e2a107d48f5c81a8c3412cac81f0ba89c4030
KevDji <kevdjidadzie@gmail.com>
2022-09-29 10:12:52 -0700

    correction

--- 0x17-doudly_linked_lists/lists.h
+++ 0x17-doudly_linked_lists/lists.h
@@ -1,9 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File: lists.h
+ * Auth: Nicolas peter wanjau
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,15 +16,14 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
-
 typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
